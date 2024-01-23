2891fc5738fd8ffee3762b7b8bb001eb836ad811
tonmoy <tonmoy3673@gmail.com>
2024-01-23 16:27:56 +0600

    create admin dashboard

---
+++ src/components/Dashboard/AdminMenu.jsx
@@ -0,0 +1,22 @@
+import { PeopleOutlineOutlined } from "@mui/icons-material";
+import { NavLink } from "react-router-dom";
+
+const AdminMenu = () => {
+  return (
+    <>
+      <NavLink
+        to="all-users"
+        className={({ isActive }) =>
+          `flex items-center px-4 py-2 mt-5  transition-colors duration-300 transform  hover:bg-gray-300   hover:text-gray-700 ${
+            isActive ? "bg-gray-300  text-white" : "text-pink-600"
+          }`
+        }
+      >
+        <PeopleOutlineOutlined className="w-5 h-5" />
+        <span className="mx-4 font-medium">All Users</span>
+      </NavLink>
+    </>
+  );
+};
+
+export default AdminMenu;

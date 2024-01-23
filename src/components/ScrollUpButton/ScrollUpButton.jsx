808b058dac307fc6da8526e8cfbcfbf2f001a91d
tonmoy <tonmoy3673@gmail.com>
2024-01-23 15:15:14 +0600

    set scroll button

--- src/components/ScrollUpButton/ScrollUpButton.jsx
+++ src/components/ScrollUpButton/ScrollUpButton.jsx
@@ -1,5 +1,52 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
 const ScrollUpButton = () => {
-  return <div></div>;
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 600) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <div
+      className={`scroll-up-btn ${isVisible ? "show" : ""}`}
+      onClick={scrollToTop}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6 inline-block"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M8.25 6.75 12 3m0 0 3.75 3.75M12 3v18"
+        />
+      </svg>
+    </div>
+  );
 };
 
 export default ScrollUpButton;

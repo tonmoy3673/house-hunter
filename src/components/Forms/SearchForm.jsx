686ee173bb5a0096f2ffaa0992fe17062ab88f25
tonmoy <tonmoy3673@gmail.com>
2024-01-23 19:10:35 +0600

    rebuild search bar

--- src/components/Forms/SearchForm.jsx
+++ src/components/Forms/SearchForm.jsx
@@ -1,135 +1,127 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { useState } from "react";
 
-const API_URL = "http://localhost:5000"; // Replace with your backend server URL
+// const API_URL = "http://localhost:5000"; // Replace with your backend server URL
 
 export const SearchForm = () => {
-  const [houses, setHouses] = useState([]);
-  const [filters, setFilters] = useState({
+  const [formData, setFormData] = useState({
     city: "",
-    minBedrooms: 0,
-    minBathrooms: 0,
-    minSize: 0,
-    availability: "",
-    rentRange: [0, Infinity],
+    bedrooms: "",
+    bathrooms: "",
+    roomSize: "",
+    availabilityDate: "",
+    rentPerMonth: "",
+    rentRange: ["", ""],
   });
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
 
-  useEffect(() => {
-    // Fetch houses from the backend when the component mounts
-    fetchHouses();
-  }, []);
-
-  const fetchHouses = async () => {
-    try {
-      setLoading(true);
-      const response = await axios.get(`${API_URL}/houses`, {
-        params: filters,
-      });
-      setHouses(response.data);
-      setError(null);
-    } catch (error) {
-      console.error("Error fetching houses:", error);
-      setError("Error fetching houses. Please try again.");
-    } finally {
-      setLoading(false);
-    }
+  const handleChange = (name, value) => {
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
   };
 
-  const handleFilterChange = (filter, value) => {
-    setFilters({ ...filters, [filter]: value });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form submitted with data:", formData);
   };
 
-  const handleSearch = () => {
-    fetchHouses();
-  };
   return (
     <div>
-      <form className="max-w-md mx-auto p-4 bg-white rounded shadow-lg">
-        <h1 className="text-2xl font-bold mb-4">House Search App</h1>
+      <form
+        onSubmit={handleSubmit}
+        className="w-2/4 mx-auto px-4 py-6 bg-white rounded shadow-lg backdrop-filter"
+      >
+        <h1 className="text-2xl font-bold mb-4 text-center">
+          House Search App
+        </h1>
 
-        <div className="mb-4">
-          <label
-            htmlFor="city"
-            className="block text-sm font-semibold text-gray-600"
-          >
-            City:
-          </label>
-          <input
-            type="text"
-            id="city"
-            value={filters.city}
-            onChange={(e) => onFilterChange("city", e.target.value)}
-            className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:border-green-500"
-          />
+        <div className="flex justify-between gap-5">
+          <div className="mb-4 lg:w-1/3">
+            <label
+              htmlFor="city"
+              className="block text-sm font-semibold text-gray-600"
+            >
+              City:
+            </label>
+            <input
+              type="text"
+              id="city"
+              name="city"
+              value={formData.city}
+              onChange={(e) => handleChange("city", e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:border-pink-500"
+            />
+          </div>
+
+          <div className="mb-4 lg:w-1/3">
+            <label
+              htmlFor="bedrooms"
+              className="block text-sm font-semibold text-gray-600"
+            >
+              Minimum Bedrooms:
+            </label>
+            <input
+              type="number"
+              id="bedrooms"
+              name="bedrooms"
+              value={formData.bedrooms}
+              onChange={(e) => handleChange("bedrooms", e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:border-pink-500"
+            />
+          </div>
+
+          <div className="mb-4 lg:w-1/3">
+            <label
+              htmlFor="bathrooms"
+              className="block text-sm font-semibold text-gray-600"
+            >
+              Minimum Bathrooms:
+            </label>
+            <input
+              type="number"
+              id="bathrooms"
+              name="bathrooms"
+              value={formData.bathrooms}
+              onChange={(e) => handleChange("bathrooms", e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:border-pink-500"
+            />
+          </div>
         </div>
 
-        <div className="mb-4">
-          <label
-            htmlFor="minBedrooms"
-            className="block text-sm font-semibold text-gray-600"
-          >
-            Minimum Bedrooms:
-          </label>
-          <input
-            type="number"
-            id="minBedrooms"
-            value={filters.minBedrooms}
-            onChange={(e) => onFilterChange("minBedrooms", e.target.value)}
-            className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:border-green-500"
-          />
-        </div>
+        <div className="flex justify-between gap-5">
+          <div className="mb-4 lg:w-1/2">
+            <label
+              htmlFor="roomSize"
+              className="block text-sm font-semibold text-gray-600"
+            >
+              Minimum Size:
+            </label>
+            <input
+              type="number"
+              id="roomSize"
+              name="roomSize"
+              value={formData.roomSize}
+              onChange={(e) => handleChange("roomSize", e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:border-pink-500"
+            />
+          </div>
 
-        <div className="mb-4">
-          <label
-            htmlFor="minBathrooms"
-            className="block text-sm font-semibold text-gray-600"
-          >
-            Minimum Bathrooms:
-          </label>
-          <input
-            type="number"
-            id="minBathrooms"
-            value={filters.minBathrooms}
-            onChange={(e) => onFilterChange("minBathrooms", e.target.value)}
-            className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:border-green-500"
-          />
-        </div>
-
-        <div className="mb-4">
-          <label
-            htmlFor="minSize"
-            className="block text-sm font-semibold text-gray-600"
-          >
-            Minimum Size:
-          </label>
-          <input
-            type="number"
-            id="minSize"
-            value={filters.minSize}
-            onChange={(e) => onFilterChange("minSize", e.target.value)}
-            className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:border-green-500"
-          />
-        </div>
-
-        <div className="mb-4">
-          <label
-            htmlFor="availability"
-            className="block text-sm font-semibold text-gray-600"
-          >
-            Availability:
-          </label>
-          <select
-            id="availability"
-            value={filters.availability}
-            onChange={(e) => onFilterChange("availability", e.target.value)}
-            className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:border-green-500"
-          >
-            <option value="">All</option>
-            <option value="true">Available</option>
-            <option value="false">Not Available</option>
-          </select>
+          <div className="mb-4 lg:w-1/2">
+            <label
+              htmlFor="availabilityDate"
+              className="block text-sm font-semibold text-gray-600"
+            >
+              Availability:
+            </label>
+            <select
+              id="availabilityDate"
+              name="availabilityDate"
+              value={formData.availabilityDate}
+              onChange={(e) => handleChange("availabilityDate", e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:border-pink-500"
+            >
+              <option value="">All</option>
+              <option value="true">Available</option>
+              <option value="false">Not Available</option>
+            </select>
+          </div>
         </div>
 
         <div className="mb-4">
@@ -143,57 +135,40 @@ export const SearchForm = () => {
             <input
               type="number"
               id="rentRangeMin"
-              value={filters.rentRange[0]}
+              name="rentRangeMin"
+              value={formData.rentRange[0]}
               onChange={(e) =>
-                onFilterChange("rentRange", [
+                handleChange("rentRange", [
                   e.target.value,
-                  filters.rentRange[1],
+                  formData.rentRange[1],
                 ])
               }
-              className="w-1/2 p-2 mr-2 border border-gray-300 rounded focus:outline-none focus:border-green-500"
+              className="w-1/2 p-2 mr-2 border border-gray-300 rounded focus:outline-none focus:border-pink-500"
             />
             <span className="text-gray-700"> - </span>
             <input
               type="number"
               id="rentRangeMax"
-              value={filters.rentRange[1]}
+              name="rentRangeMax"
+              value={formData.rentRange[1]}
               onChange={(e) =>
-                onFilterChange("rentRange", [
-                  filters.rentRange[0],
+                handleChange("rentRange", [
+                  formData.rentRange[0],
                   e.target.value,
                 ])
               }
-              className="w-1/2 p-2 ml-2 border border-gray-300 rounded focus:outline-none focus:border-green-500"
+              className="w-1/2 p-2 ml-2 border border-gray-300 rounded focus:outline-none focus:border-pink-500"
             />
           </div>
         </div>
 
         <button
           type="submit"
-          className="w-full p-2 bg-green-500 text-white rounded hover:bg-green-600 transition duration-300"
+          className="w-full p-2 bg-pink-500 text-white rounded hover:bg-pink-600 transition duration-300"
         >
           Search
         </button>
       </form>
-
-      {loading && <p>Loading...</p>}
-      {error && <p style={{ color: "red" }}>{error}</p>}
-      {!loading && !error && <SearchResultList houses={houses} />}
-    </div>
-  );
-};
-
-const SearchResultList = ({ houses }) => {
-  return (
-    <div>
-      <h2>Search Results:</h2>
-      <ul>
-        {houses.map((house, index) => (
-          <li key={index}>
-            <p>{`City: ${house.city}, Bedrooms: ${house.bedrooms}, Bathrooms: ${house.bathrooms}, Size: ${house.size}, Availability: ${house.availability}, Rent: ${house.rent}`}</p>
-          </li>
-        ))}
-      </ul>
     </div>
   );
 };

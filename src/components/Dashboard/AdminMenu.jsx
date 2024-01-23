25d01b84f75b3e25071ceb3ca759a0e4148ad124
tonmoy <tonmoy3673@gmail.com>
2024-01-23 16:32:17 +0600

    complete admin dashboard

--- src/components/Dashboard/AdminMenu.jsx
+++ src/components/Dashboard/AdminMenu.jsx
@@ -1,4 +1,4 @@
-import { PeopleOutlineOutlined } from "@mui/icons-material";
+import { GroupAddOutlined, PeopleOutlineOutlined } from "@mui/icons-material";
 import { NavLink } from "react-router-dom";
 
 const AdminMenu = () => {
@@ -14,6 +14,18 @@ const AdminMenu = () => {
       >
         <PeopleOutlineOutlined className="w-5 h-5" />
         <span className="mx-4 font-medium">All Users</span>
+      </NavLink>
+      <NavLink
+        to="all-bookings"
+        className={({ isActive }) =>
+          `flex items-center px-4 py-2 mt-5  transition-colors duration-300 transform  hover:bg-gray-300   hover:text-gray-700 ${
+            isActive ? "bg-gray-300  text-white" : "text-pink-600"
+          }`
+        }
+      >
+        <GroupAddOutlined className="w-5 h-5" />
+
+        <span className="mx-4 font-medium">All of Bookings</span>
       </NavLink>
     </>
   );

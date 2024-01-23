c7fb3f0050f4c088afab06c2c0748b1c72418630
tonmoy <tonmoy3673@gmail.com>
2024-01-23 12:11:07 +0600

    set bg image

---
+++ src/components/Home/Hero.jsx
@@ -0,0 +1,15 @@
+import { SearchForm } from "../Forms/SearchForm";
+
+const Hero = () => {
+  return (
+    <div>
+      <div className="py-40 bg-[url('https://d23e6qdu75bsi3.cloudfront.net/webpack/images/skyline-08-vegas-2dddf6f764f9375b4972.jpg')] bg-no-repeat bg-center bg-cover">
+        <div>
+          <SearchForm></SearchForm>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;

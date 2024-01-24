aca40c229330835a17ac03e8db46c3605cbef5db
tonmoy <tonmoy3673@gmail.com>
2024-01-24 12:21:24 +0600

    polish signin & singup

--- src/Pages/SignIn/SignIn.jsx
+++ src/Pages/SignIn/SignIn.jsx
@@ -1,17 +1,48 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import { login } from "../../api/user";
+import { AuthContext } from "../../contexts/AuthProvider";
 
-const SignIn = () => {
+const SigninPage = () => {
+  const { signin } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const location = useLocation();
   const [formData, setFormData] = useState({
     email: "",
     password: "",
     showPassword: false,
   });
+  const [errors, setErrors] = useState({});
+
+  const from = location?.state?.from.pathname || "/dashboard";
+
+  const validateForm = () => {
+    const newErrors = {};
+
+    if (!formData.email.trim()) {
+      newErrors.email = "Email is required";
+    }
+
+    if (!formData.password.trim()) {
+      newErrors.password = "Password is required";
+    }
+
+    setErrors(newErrors);
+
+    // Return true if the form is valid, false otherwise
+    return Object.keys(newErrors).length === 0;
+  };
 
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+    // Clear the error message when the user starts typing
+    setErrors((prevErrors) => ({ ...prevErrors, [name]: "" }));
   };
 
   const handleTogglePassword = () => {
@@ -20,68 +51,95 @@ const SignIn = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const { email, password } = formData;
-    console.log(email, password);
-    // try {
-    //   const response = await axios.post('http://localhost:5000/login', formData);
-    //   const token = response.data.token;
-    //   console.log('Login successful, token:', token);
-    //   // Store the token in localStorage or a secure storage mechanism
-    //   // Redirect to the dashboard or handle success accordingly
-    // } catch (error) {
-    //   console.error('Login failed', error.response.data.message);
-    // }
+
+    if (validateForm()) {
+      try {
+        await signin(formData.email, formData.password);
+        navigate(from, { replace: true });
+        const data = {
+          email: formData.email,
+          password: formData.password,
+        };
+        login(data);
+      } catch (error) {
+        console.error("Login failed:", error);
+        setErrors((prevErrors) => ({
+          ...prevErrors,
+          general: "Invalid credentials. Please try again.",
+        }));
+      }
+    }
   };
+
   return (
-    <div className="container mx-auto mt-20 mb-20">
+    <div className="container mx-auto pt-20 mb-20">
       <div className="p-8 mx-auto lg:w-6/12">
         <form
           onSubmit={handleSubmit}
           className="bg-gray-100 shadow-md px-8 pb-20 pt-10 mb-4 max-w-md mx-auto"
         >
           <h1 className="text-2xl font-semibold text-center mb-10">Sign In</h1>
-          <label
-            className="block text-gray-700 text-sm font-bold mb-3"
-            htmlFor="fullName"
-          >
-            Email:
-            <input
-              type="email"
-              className="w-full border p-2 mb-4 rounded"
-              placeholder="Email"
-              required
-              value={formData.email}
-              name="email"
-              onChange={handleChange}
-            />
-          </label>
-          <div className="relative">
+          {errors.general && (
+            <p className="text-red-500 text-xs italic mb-3">{errors.general}</p>
+          )}
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-3"
+              htmlFor="email"
+            >
+              Email:
+              <input
+                type="email"
+                className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+                  errors.email ? "border-red-500" : ""
+                }`}
+                placeholder="Email"
+                required
+                value={formData.email}
+                name="email"
+                onChange={handleChange}
+              />
+              {errors.email && (
+                <p className="text-red-500 text-xs italic mt-1">
+                  {errors.email}
+                </p>
+              )}
+            </label>
+          </div>
+          <div className="relative mb-4">
             <label
               className="block text-gray-700 text-sm font-bold"
-              htmlFor="fullName"
+              htmlFor="password"
             >
               Password:
               <input
                 type={formData.showPassword ? "text" : "password"}
-                className="w-full border p-2 mb-4 rounded"
+                className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+                  errors.password ? "border-red-500" : ""
+                }`}
                 placeholder="Password"
                 required
                 name="password"
                 value={formData.password}
                 onChange={handleChange}
               />
+              <button
+                type="button"
+                onClick={handleTogglePassword}
+                className="absolute right-3 top-6"
+              >
+                {formData.showPassword ? (
+                  <VisibilityOffIcon />
+                ) : (
+                  <VisibilityIcon />
+                )}
+              </button>
             </label>
-            <button
-              type="button"
-              onClick={handleTogglePassword}
-              className="absolute right-3 top-6"
-            >
-              {formData.showPassword ? (
-                <VisibilityOffIcon />
-              ) : (
-                <VisibilityIcon />
-              )}
-            </button>
+            {errors.password && (
+              <p className="text-red-500 text-xs italic mt-1">
+                {errors.password}
+              </p>
+            )}
           </div>
           <button
             type="submit"
@@ -91,7 +149,7 @@ const SignIn = () => {
           </button>
         </form>
         <p className="text-center mt-4">
-          Do not have an account?{" "}
+          Don't have an account?{" "}
           <Link to="/create-account" className="mr-5 text-blue-500">
             <button className="rounded-full px-2 py-1">Signup</button>
           </Link>
@@ -101,4 +159,4 @@ const SignIn = () => {
   );
 };
 
-export default SignIn;
+export default SigninPage;
--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,17 +1,27 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { register } from "../../api/user";
+import { AuthContext } from "../../contexts/AuthProvider";
 
 const SignupPage = () => {
-  const [isLogin, setIsLogin] = useState(false);
+  const { createUser } = useContext(AuthContext);
+
   const initialFormData = {
     fullName: "",
     role: "",
     phoneNumber: "",
     email: "",
     password: "",
+    imageUrl:
+      "https://www.iconpacks.net/icons/1/free-user-login-icon-305-thumb.png",
   };
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const from = location?.state?.from.pathname || "/dashboard";
 
   const [formData, setFormData] = useState(initialFormData);
+  const [errors, setErrors] = useState({});
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -19,18 +29,67 @@ const SignupPage = () => {
       ...prevData,
       [name]: value,
     }));
+    // Clear the error message when the user starts typing
+    setErrors((prevErrors) => ({ ...prevErrors, [name]: "" }));
   };
 
-  const handleSubmit = (e) => {
+  const validateForm = () => {
+    const newErrors = {};
+
+    // Validate each field
+    if (!formData.fullName.trim()) {
+      newErrors.fullName = "Full Name is required";
+    }
+
+    if (!formData.role) {
+      newErrors.role = "Role is required";
+    }
+
+    if (!formData.phoneNumber.trim()) {
+      newErrors.phoneNumber = "Phone Number is required";
+    }
+
+    if (!formData.email.trim()) {
+      newErrors.email = "Email is required";
+    }
+
+    if (!formData.password.trim()) {
+      newErrors.password = "Password is required";
+    } else {
+      // Password validation
+      const passwordRegex =
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/;
+      if (!passwordRegex.test(formData.password)) {
+        newErrors.password =
+          "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character.";
+      }
+    }
+
+    setErrors(newErrors);
+
+    // Return true if the form is valid, false otherwise
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    console.log("Form submitted:", formData);
-
-    setFormData(initialFormData);
+    if (validateForm()) {
+      createUser(formData.email, formData.password)
+        .then((result) => {
+          register(formData);
+          navigate(from, { replace: true });
+          setFormData(initialFormData);
+        })
+        .catch((error) => {
+          // Handle registration error
+          console.error("Registration error:", error);
+        });
+    }
   };
 
   return (
-    <div className="container mx-auto mt-20 mb-20">
+    <div className="container mx-auto pt-20 mb-20">
       <div className="p-8 mx-auto lg:w-6/12">
         <form
           onSubmit={handleSubmit}
@@ -49,8 +108,13 @@ const SignupPage = () => {
                 name="fullName"
                 value={formData.fullName}
                 onChange={handleChange}
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+                  errors.fullName ? "border-red-500" : ""
+                }`}
               />
+              {errors.fullName && (
+                <p className="text-red-500 text-xs italic">{errors.fullName}</p>
+              )}
             </label>
           </div>
           <div className="mb-4">
@@ -64,12 +128,17 @@ const SignupPage = () => {
                 name="role"
                 value={formData.role}
                 onChange={handleChange}
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+                  errors.role ? "border-red-500" : ""
+                }`}
               >
                 <option value="">Select Role</option>
                 <option value="House Owner">House Owner</option>
                 <option value="House Renter">House Renter</option>
               </select>
+              {errors.role && (
+                <p className="text-red-500 text-xs italic">{errors.role}</p>
+              )}
             </label>
           </div>
           <div className="mb-4">
@@ -84,8 +153,15 @@ const SignupPage = () => {
                 name="phoneNumber"
                 value={formData.phoneNumber}
                 onChange={handleChange}
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+                  errors.phoneNumber ? "border-red-500" : ""
+                }`}
               />
+              {errors.phoneNumber && (
+                <p className="text-red-500 text-xs italic">
+                  {errors.phoneNumber}
+                </p>
+              )}
             </label>
           </div>
           <div className="mb-4">
@@ -100,8 +176,13 @@ const SignupPage = () => {
                 name="email"
                 value={formData.email}
                 onChange={handleChange}
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+                  errors.email ? "border-red-500" : ""
+                }`}
               />
+              {errors.email && (
+                <p className="text-red-500 text-xs italic">{errors.email}</p>
+              )}
             </label>
           </div>
           <div className="mb-6">
@@ -116,8 +197,13 @@ const SignupPage = () => {
                 name="password"
                 value={formData.password}
                 onChange={handleChange}
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+                  errors.password ? "border-red-500" : ""
+                }`}
               />
+              {errors.password && (
+                <p className="text-red-500 text-xs italic">{errors.password}</p>
+              )}
             </label>
           </div>
           <div className="flex items-center justify-between">
@@ -132,7 +218,7 @@ const SignupPage = () => {
         <p className="text-center mt-4">
           Already have an account?{" "}
           <Link to="/login" className="mr-5 text-blue-500">
-            <button className="rounded-full px-2 py-1">Signup</button>
+            <button className="rounded-full px-2 py-1">Login</button>
           </Link>
         </p>
       </div>
